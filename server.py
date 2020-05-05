f09a31f3ef34f7ec6fa60976ca853c403aba2f91
PatilChetana <38126001+PatilChetana@users.noreply.github.com>
2020-05-05 23:39:48 +0530

    server

--- server.py
+++ server.py
@@ -23,20 +23,6 @@ app = Flask(__name__)
 
 
 
-classifier = Sequential()
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-classifier.add(Flatten())
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
-classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-train_datagen = ImageDataGenerator(rescale = 1. / 255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
-test_datagen = ImageDataGenerator(rescale = 1. / 255)
-training_set = train_datagen.flow_from_directory('models/training_set', target_size = (64, 64), batch_size = 8, class_mode = 'binary')
-test_set = test_datagen.flow_from_directory('models/test_set', target_size = (64, 64), batch_size = 8, class_mode = 'binary')
-classifier.fit_generator(training_set, steps_per_epoch  = 24, epochs = 2, validation_data = test_set, validation_steps = 1000)
 
 #training phases below
 
@@ -46,6 +32,21 @@ classifier.fit_generator(training_set, steps_per_epoch  = 24, epochs = 2, valida
 #testing phase below  (run this code everytime, donot run the whole code)
 
 def predict():
+  classifier = Sequential()
+  classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+  classifier.add(MaxPooling2D(pool_size=(2, 2)))
+  classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+  classifier.add(MaxPooling2D(pool_size=(2, 2)))
+  classifier.add(Flatten())
+  classifier.add(Dense(units = 1, activation = 'sigmoid'))
+  classifier.compile(optimizer='adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+  from tensorflow.keras.preprocessing.image import ImageDataGenerator
+  train_datagen = ImageDataGenerator(rescale = 1. / 255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+  test_datagen = ImageDataGenerator(rescale = 1. / 255)
+  training_set = train_datagen.flow_from_directory('models/training_set', target_size = (64, 64), batch_size = 8, class_mode = 'binary')
+  test_set = test_datagen.flow_from_directory('models/test_set', target_size = (64, 64), batch_size = 8, class_mode = 'binary')
+  classifier.fit_generator(training_set, steps_per_epoch  = 24, epochs = 2, validation_data = test_set, validation_steps = 1000)
+
   import numpy as np
   from tensorflow.keras.preprocessing import image
   test_image = image.load_img('models/sample3.jpg', target_size = (64, 64))   #file to be predicted imagename
