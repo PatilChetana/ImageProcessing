0644fff6f4914eade5790cc88e8c3e395369f350
PatilChetana <38126001+PatilChetana@users.noreply.github.com>
2020-05-09 20:18:23 +0530

    server

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-import nltk
+#import nltk
 import numpy as np
-import random
-import string # to process standard python strings
+#import random
+#import string # to process standard python strings
 from werkzeug.utils import secure_filename           #for image input
 import os
 from tensorflow.keras.models import Sequential, model_from_json, load_model
@@ -14,119 +14,13 @@ from tensorflow.keras.layers import Dense
 
 
 
-import sys
-print("Python version")
-print (sys.version)
+
 
 app = Flask(__name__)
 
 
 
-f=open('chatbot.txt','r',errors = 'ignore')
-raw=f.read()
-
-
-raw=raw.lower()# converts to lowercase
-nltk.download('punkt') # first-time use for english
-nltk.download('wordnet') # first-time use only
-sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
-word_tokens = nltk.word_tokenize(raw)# converts to list of words
-
-lemmer = nltk.stem.WordNetLemmatizer()
-
-
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
-	
-
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
-
-
-def greeting(sentence):
-    for word in sentence.split():
-        if word.lower() in GREETING_INPUTS:
-            return random.choice(GREETING_RESPONSES)
-			
-		
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def get_bot_response(user_response):
-    robo_response=''
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    print(sent_tokens)
-    tfidf = TfidfVec.fit_transform(sent_tokens)
-    vals = cosine_similarity(tfidf[-1], tfidf)
-    print(tfidf[-1])   								 #userinput tfidf
-    print("*********************")
-    print(tfidf)       								 #tfidf of whole database
-    print("Cosine ")
-    print(vals)       							     #cosine similarity value of every document
-    idx=vals.argsort()[0][-2]						 #second largest cosine similarity value's index is stored in idx
-    print(idx)										 #idx
-    flat = vals.flatten()							 #converts 2d array into 1d array
-    print("flat")
-    print(flat)                                      #1d array
-    flat.sort()
-    req_tfidf = flat[-2]							# second largest tfidf
-    print("REQ tifid")
-    print(req_tfidf)
-    if(req_tfidf==0):
-        robo_response=robo_response+"I am sorry! I don't understand you"
-        return robo_response
-    else:
-        robo_response = robo_response+sent_tokens[idx]
-        return robo_response
-
-def text_response(user_response):		
- flag=True
- print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
- while(flag==True):
-    print(user_response)
-    #print(type(user_response))
-    user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
-            print("ROBO: You are welcome..")
-        else:
-            if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
-                return greeting(user_response)
-            else:
-                word_tokens=[]
-                sent_tokens.append(user_response)
-                word_tokens+=nltk.word_tokenize(user_response)
-                final_words=list(set(word_tokens))
-                print(word_tokens)
-                print("******************************")
-                print(final_words)
-                print("ROBO: ",end="")
-                global output
-                output=str(get_bot_response(user_response))
-                print(output)
-                print(type(output))
-                sent_tokens.remove(user_response)
-                return output
-				
-    else:
-        flag=False
-        print("ROBO: Bye! take care..")
-
-
-def fetchnumber(output):
-  test_string =output
-  print("The original string : " + test_string) 
-  #res = [int(i) for i in test_string.split() if i.isdigit()] 
-  for i in test_string.split():
-        if i.isdigit():
-            res=int(i)
-  print("The number  is : "+str(res)) 
-  return res
+   
   
 '''from twilio.rest import Client as Call
 
@@ -165,38 +59,13 @@ def imageprocess(imagename):
     else:
        prediction = 'Smoke'
 	
-    return result
+    return prediction
 
 #define app routes
 @app.route("/")
 def index():
     return render_template("index.html")
 	
-@app.route("/get")
-#function for the bot response
-def text():
-     input=request.args.get('msg')
-     #print(input)
-     return text_response(input)
-    
- 
-@app.route('/voice')
-def get_voice():
-     input=request.args.get('voice')
-     #print(input)
-     return text_response(input)
-
-
-@app.route('/call', methods=['POST'])
-def calls():
-    print(request.method)
-    print(output)
-    if request.method=="POST":
-     number=fetchnumber(output)
-     #Calling(number)
-     #return "Call Successful"
-     return render_template("mo.html",value=number)
- 
 
 
 
